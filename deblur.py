ed635164458e95b1ef26454bf822c568409fc898
Azad khan <54736577+imazk@users.noreply.github.com>
2022-12-19 19:37:44 +0530

    Create deblur.py

---
+++ deblur.py
@@ -0,0 +1,203 @@
+import os
+import matplotlib.pyplot as plt
+import cv2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import time
+import argparse
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import transforms
+from torchvision.utils import save_image
+from sklearn.model_selection import train_test_split
+
+# constructing the argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--epochs', type=int, default=40,
+                    help='number of epochs to train the model for')
+args = vars(parser.parse_args())
+
+# helper functions
+image_dir = '../outputs/saved_images'
+os.makedirs(image_dir, exist_ok=True)
+
+
+def save_decoded_image(img, name):
+    img = img.view(img.size(0), 3, 224, 224)
+    save_image(img, name)
+
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+print(device)
+
+batch_size = 2
+
+gauss_blur = os.listdir('../input/gaussian_blurred')
+gauss_blur.sort()
+sharp = os.listdir('../input/sharp')
+sharp.sort()
+
+x_blur = []
+for i in range(len(gauss_blur)):
+    x_blur.append(gauss_blur[i])
+
+y_sharp = []
+for i in range(len(sharp)):
+    y_sharp.append(sharp[i])
+
+(x_train, x_val, y_train, y_val) = train_test_split(x_blur, y_sharp, test_size=0.25)
+
+print(f"Train data instances: {len(x_train)}")
+print(f"Validation data instances: {len(x_val)}")
+
+# define transforms
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+
+class DeblurDataset(Dataset):
+    def __init__(self, blur_paths, sharp_paths=None, transforms=None):
+        self.X = blur_paths
+        self.y = sharp_paths
+        self.transforms = transforms
+
+    def __len__(self):
+        return (len(self.X))
+
+    def __getitem__(self, i):
+        blur_image = cv2.imread(f"../input/gaussian_blurred/{self.X[i]}")
+
+        if self.transforms:
+            blur_image = self.transforms(blur_image)
+
+        if self.y is not None:
+            sharp_image = cv2.imread(f"../input/sharp/{self.y[i]}")
+            sharp_image = self.transforms(sharp_image)
+            return (blur_image, sharp_image)
+        else:
+            return blur_image
+
+
+train_data = DeblurDataset(x_train, y_train, transform)
+val_data = DeblurDataset(x_val, y_val, transform)
+
+trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+valloader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
+
+
+class DeblurCNN(nn.Module):
+    def __init__(self):
+        super(DeblurCNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=2)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=2)
+        self.conv3 = nn.Conv2d(32, 3, kernel_size=5, padding=2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.conv3(x)
+
+        return x
+
+
+model = DeblurCNN().to(device)
+print(model)
+
+# the loss function
+criterion = nn.MSELoss()
+# the optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer,
+    mode='min',
+    patience=5,
+    factor=0.5,
+    verbose=True
+)
+
+
+# the training function
+def fit(model, dataloader, epoch):
+    model.train()
+    running_loss = 0.0
+    for i, data in tqdm(enumerate(dataloader), total=int(len(train_data) / dataloader.batch_size)):
+        blur_image = data[0]
+        sharp_image = data[1]
+        blur_image = blur_image.to(device)
+        sharp_image = sharp_image.to(device)
+        optimizer.zero_grad()
+        outputs = model(blur_image)
+        loss = criterion(outputs, sharp_image)
+        # backpropagation
+        loss.backward()
+        # update the parameters
+        optimizer.step()
+        running_loss += loss.item()
+
+    train_loss = running_loss / len(dataloader.dataset)
+    print(f"Train Loss: {train_loss:.5f}")
+
+    return train_loss
+
+
+# the validation function
+def validate(model, dataloader, epoch):
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for i, data in tqdm(enumerate(dataloader), total=int(len(val_data) / dataloader.batch_size)):
+            blur_image = data[0]
+            sharp_image = data[1]
+            blur_image = blur_image.to(device)
+            sharp_image = sharp_image.to(device)
+            outputs = model(blur_image)
+            loss = criterion(outputs, sharp_image)
+            running_loss += loss.item()
+
+            if epoch == 0 and i == int((len(val_data) / dataloader.batch_size) - 1):
+                save_decoded_image(sharp_image.cpu().data, name=f"../outputs/saved_images/sharp{epoch}.jpg")
+                save_decoded_image(blur_image.cpu().data, name=f"../outputs/saved_images/blur{epoch}.jpg")
+
+            if i == int((len(val_data) / dataloader.batch_size) - 1):
+                save_decoded_image(outputs.cpu().data, name=f"../outputs/saved_images/val_deblurred{epoch}.jpg")
+
+        val_loss = running_loss / len(dataloader.dataset)
+        print(f"Val Loss: {val_loss:.5f}")
+
+        return val_loss
+
+
+train_loss = []
+val_loss = []
+start = time.time()
+for epoch in range(args['epochs']):
+    print(f"Epoch {epoch + 1} of {args['epochs']}")
+    train_epoch_loss = fit(model, trainloader, epoch)
+    val_epoch_loss = validate(model, valloader, epoch)
+    train_loss.append(train_epoch_loss)
+    val_loss.append(val_epoch_loss)
+    scheduler.step(val_epoch_loss)
+end = time.time()
+
+print(f"Took {((end - start) / 60):.3f} minutes to train")
+
+# loss plots
+plt.figure(figsize=(10, 7))
+plt.plot(train_loss, color='orange', label='train loss')
+plt.plot(val_loss, color='red', label='validataion loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('../outputs/loss.png')
+plt.show()
+
+# save the model to disk
+print('Saving model...')
+torch.save(model.state_dict(), '../outputs/model.pth')
